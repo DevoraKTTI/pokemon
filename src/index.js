07246e8447e7b1b5e5e0f670f0f6e7c275e8b1c8
DevoraKuperman <devoraktti@gmail.com>
2024-08-12 14:51:04 -0400

    Pokemon app -- with a nav bar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import AppRoute from './AppRoute';
 import reportWebVitals from './reportWebVitals';
+import NavbarPage from './NavbarPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavbarPage/>
+    <AppRoute />
   </React.StrictMode>
 );
 
